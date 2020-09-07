c6ac8eac8c8260a1294ec86963dd911b6c964d23
MeckeJadeHS <mecke@jade-hs.de>
2020-09-07 16:57:06 +0200

    Erste Aufgabe mit Unittest

---
+++ BubbleSort-Unittesting/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BubbleSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BubbleSort_Unittesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Teste4Elemente()
+        {
+            // Arrange
+            Program prg = new Program();
+
+            // Act 
+            var result = prg.bubbleSort(new List<int>() { 3, 1, 6, 4 });
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new List<int>() { 1, 3, 4, 6 }, result));              
+        }
+
+        [TestMethod]
+        public void Teste6Elemente()
+        {
+            // Arrange
+            Program prg = new Program();
+
+            // Act 
+            var result = prg.bubbleSort(new List<int>() { 3, 1, 6, 4, 1, 9 });
+
+            // Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(new List<int>() { 1, 1, 3, 4, 6, 9 }, result));
+        }
+
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    public class Program
+    {
+
+        public List<int> bubbleSort(List<int> liste)
+        {
+            // bubbleSort hier
+            throw new NotImplementedException();
+            
+            return liste;
+        }
+
+        static void Main(string[] args)
+        {
+            Program prg = new Program();
+            Console.WriteLine(prg.bubbleSort(new List<int>() { 3, 5, 1, 2 }));
+            Console.ReadKey();
+        }
+
+    }
+}
